df0a6384e80b9aa7ad9563bf00ee454939a32083
Alberto Abate <alberto.abate@gmail.com>
2026-02-10 10:27:28 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,14 @@
+import React, { useState } from 'react';
+import { createRoot } from 'react-dom/client';
+import Rectangulo from './components/Rectangulo.js';
 
+function App() {
+  return (
+    <div>
+      <Rectangulo />
+    </div>
+  );
+}
+
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
