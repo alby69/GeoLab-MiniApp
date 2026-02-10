bf3d514145c4111fea707d96590c536d5e6b02ed
Alberto Abate <alberto.abate@gmail.com>
2026-02-10 10:29:02 +0100

    Add files via upload

--- src/components/Triangolo.js
+++ src/components/Triangolo.js
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 
-export default function Rectangulo() {
+export default function Triangolo() {
   const [base, setBase] = useState(4);
   const [altezza, setAltezza] = useState(3);
-  const area = base * altezza;
+  const area = (base * altezza) / 2;
 
   return (
     <div style={{ padding: '10px', border: '1px solid black', margin: '10px' }}>
-      <h2>Rettangolo</h2>
+      <h2>Triangolo</h2>
       <p>Base: {base}, Altezza: {altezza}</p>
       <p>Area: {area}</p>
     </div>
